65748d23095d2da201c698c9bcb684317025a563
Matheus Pesch Zenere <mpzenere@gmail.com>
2026-05-15 03:11:14 -0300

    Created Login feature

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\Login;
 use App\Http\Controllers\Auth\Logout;
 use App\Http\Controllers\Auth\Register;
 use App\Http\Controllers\ChirpController;
@@ -19,9 +20,19 @@ Route::middleware('auth')->group(function () {
 Route::view('/register', 'auth.register')
     ->middleware('guest')
     ->name('register');
+
 Route::post('/register', Register::class)
     ->middleware('guest');
 
 // LOGOUT
 Route::post('/logout', Logout::class)
-    ->middleware('auth');
+    ->middleware('auth')
+    ->name('logout');
+
+// LOGIN
+Route::view('/login', 'auth.login')
+    ->middleware('guest')
+    ->name('login');
+
+Route::post('/login', Login::class)
+    ->middleware('guest');
